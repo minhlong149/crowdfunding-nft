862c798f8e457564dff6629bbb029c0a7dbc70a4
Long Nguyen <86862856+minhlong149@users.noreply.github.com>
2024-04-03 18:48:33 +0700

    Implement release functionality
    fixup! Replace project details props with context

--- nextjs/src/components/ProjectDetails/ReleaseProject.tsx
+++ nextjs/src/components/ProjectDetails/ReleaseProject.tsx
@@ -1,28 +1,58 @@
 "use client";
 
 import { ProjectContext } from "@/lib/context";
-import { useContext } from "react";
+import { useWriteCrowdfundingNftReleaseProject } from "@/lib/contracts";
+import { FormEvent, useContext, useEffect } from "react";
 import { useAccount } from "wagmi";
 
 export default function ReleaseProject() {
-  const { project } = useContext(ProjectContext);
+  const { project, reloadProject } = useContext(ProjectContext);
   const { address } = useAccount();
+  const { writeContract, data, error, isPending } =
+    useWriteCrowdfundingNftReleaseProject();
+
+  useEffect(() => {
+    if (!error) {
+      reloadProject();
+    }
+  }, [data]);
 
   if (project.released) {
     // TODO: Render the NFT based on its type (image, video, etc.)
+    return <p>Project released!</p>;
   }
 
   if (address !== project.owner) {
     return <></>;
   }
 
-  function handleRelease() {
-    // TODO: Allow owner to release their projects
+  if (project.fund < project.goal) {
+    return <></>;
+  }
+
+  function handleRelease(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const assetUri = formData.get("assetUri") as string;
+
+    writeContract({ args: [project.id, assetUri] });
+
+    e.currentTarget.reset();
   }
 
   return (
-    <div>
-      <button onClick={handleRelease}>Release project</button>
-    </div>
+    <form onSubmit={handleRelease}>
+      <fieldset role="group">
+        <input
+          name="assetUri"
+          placeholder="URI of the project's asset"
+          required
+        />
+        <input type="submit" value="Release" disabled={isPending} />
+      </fieldset>
+
+      {error && <p>Error: {error.message}</p>}
+    </form>
   );
 }
