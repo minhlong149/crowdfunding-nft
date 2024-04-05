b1c504ea1fab3c2ad54da2ea6a3fdb31563219f5
Long Nguyen <86862856+minhlong149@users.noreply.github.com>
2024-04-05 18:34:50 +0700

    Add file upload functionality to ReleaseProject component

--- nextjs/src/components/ProjectDetails/ReleaseProject.tsx
+++ nextjs/src/components/ProjectDetails/ReleaseProject.tsx
@@ -32,15 +32,27 @@ export default function ReleaseProject() {
     return <></>;
   }
 
-  function handleRelease(e: FormEvent<HTMLFormElement>) {
+  async function handleRelease(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
-    const assetUri = formData.get("assetUri") as string;
+    let assetUri = formData.get("assetUri") as string | File;
+
+    if (assetUri instanceof File) {
+      assetUri = await uploadFile(assetUri);
+    }
+
+    // TODO: Check if the asset URI is a valid IPFS CID
 
     writeContract({ args: [project.id, assetUri] });
 
     e.currentTarget.reset();
+  }
+
+  async function uploadFile(file: File): Promise<string> {
+    // TODO: Send a POST request to the IPFS API to upload the file
+    //  The API will return the CID of the uploaded file
+    return "bafybeicn7i3soqdgr7dwnrwytgq4zxy7a5jpkizrvhm5mv6bgjd32wm3q4/welcome-to-IPFS.jpg";
   }
 
   return (
